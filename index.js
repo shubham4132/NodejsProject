38627aeb1e379ade50e6c757272c68a3039c26a1
shubham4132 <shubhamg3122134@gmail.com>
2024-03-21 14:56:31 +0530

    learning ejs

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
+import path from "path";
 //making the server
 const app = express();
+//static folder is served
+app.use(express.static(path.join(path.resolve(), "public")));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   // res.send("hi");
-  res.sendStatus(404);
+  res.render("index", { name: "shubham" });
 });
 app.listen(5000, () => {
   console.log("server is working");
